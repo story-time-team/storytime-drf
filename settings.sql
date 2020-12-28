f934c3da90460a702f3b7d36e5903e45532a0968
AshveerB <ashveerbhayroo@gmail.com>
2020-12-28 11:56:05 -0800

    first commit

---
+++ settings.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE storytime;
+CREATE USER storytimeuser WITH PASSWORD 'storytime';
+GRANT ALL PRIVILEGES ON DATABASE storytime TO storytimeuser;
+
---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(verbose_name='email',
+                              max_length=255, unique=True)
+    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = 'email'
+
+    def get_username(self):
+        return self.email
